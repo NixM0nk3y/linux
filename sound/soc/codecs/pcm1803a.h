e5429f2c99dc6cb5e8f59fefe7162e2bbe3389ef
Nick Gregory <nick@openenterprise.co.uk>
2014-07-30 17:54:18 +0000

    add header

---
+++ sound/soc/codecs/pcm1803a.h
@@ -0,0 +1,20 @@
+#ifndef __PCM1803A_H__
+#define __PCM1803A_H__
+
+enum pcm1803a_pll_src {
+        PCM1803A_PLL_SRC_MCLK1 = 0,
+};
+
+enum pcm1803a_pll {
+        PCM1803A_PLL1 = 0,
+};
+
+enum pcm1803a_clk_src {
+        PCM1803A_CLK_SRC_PLL1 = 0,
+};
+
+enum pcm1803a_clk_div {
+        PCM1803A_CLKGDV = 0,
+};
+
+#endif
